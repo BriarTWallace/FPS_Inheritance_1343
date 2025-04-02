aad2bd254d20f6b0fe81a972536cd4c4c7a7b03a
Briar <Briar.T.Wallace@gmail.com>
2025-04-02 16:11:33 -0500

    First Two Done

--- Assets/Automatic.cs
+++ Assets/Automatic.cs
@@ -2,18 +2,36 @@ using UnityEngine;
 
 public class Automatic : Gun
 {
+    [SerializeField] GameObject particleEffect;
     public override bool AttemptFire()
     {
         if (!base.AttemptFire())
             return false;
 
         var b = Instantiate(bulletPrefab, gunBarrelEnd.transform.position, gunBarrelEnd.rotation);
-        b.GetComponent<Projectile>().Initialize(1, 200, 2, 1, null); // version without special effect
+        b.GetComponent<Projectile>().Initialize(1, 200, 2, 1, HitUpward); // version without special effect
 
         anim.SetTrigger("shoot");
         elapsed = 0;
         ammo -= 1;
 
         return true;
+    }
+
+    // example function, make hit enemy fly upward
+    void HitUpward(HitData data)
+    {
+        Vector3 impactLocation = data.location;
+
+        Instantiate(particleEffect, impactLocation, Quaternion.identity);
+
+        var colliders = Physics.OverlapSphere(impactLocation, 10);
+        foreach (var c in colliders)
+        {
+            if (c.GetComponent<Rigidbody>())
+            {
+                c.GetComponent<Rigidbody>().AddForce(Vector3.up * 20, ForceMode.Impulse);
+            }
+        }
     }
 }
